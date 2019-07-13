5886873fde7a55ea93f6cf494250c8a775dcb5c5
sagar ghimire <31683330+iamsagar7@users.noreply.github.com>
2019-07-13 09:45:41 +0545

    Update logService.js

--- src/services/logService.js
+++ src/services/logService.js
@@ -1,6 +1,4 @@
-// For simplicity, I changed the implementation of this module
-// and removed Raven. We can always add that in the future
-// and this module is the only module we need to modify.
+//raben can be used for log
 
 function init() {}
 
